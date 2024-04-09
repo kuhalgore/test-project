a13eed2396476e3312cca30bfc9b60ec6017e0fa
kuhalgore <kushal.gore.cpp@gmail.com>
2024-04-09 10:38:07 +0530

    first commit - sample main function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-project
 My First project
+To understand - how the things work
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+int main(int argc, char *argv[])
+{
+	std::string inputFile{};
+	if (argc == 1)
+	{
+		std::cout << "No arguments passed, \n";
+	}
+	else
+	{
+		std::cout<<"Logic goes here based on the args \n";
+	}
+	
+	return 0;
+}
